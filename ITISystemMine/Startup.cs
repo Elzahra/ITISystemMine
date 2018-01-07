1615a5c75bbee585121b7cdb024ebafc759e17c1
Zahraa <Zahraa@DESKTOP-8F1C3EU>
2018-01-07 04:41:42 +0200

    Add project files.

---
+++ ITISystemMine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITISystemMine.Startup))]
+namespace ITISystemMine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
